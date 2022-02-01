9e1b79e8cdbe224c01367123da776da9cc5e25af
segundevs <tushbash@gmail.com>
2022-02-01 01:26:20 -0800

    implemented private route

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,15 @@
+import { Redirect, Route } from "react-router-dom";
+import { useAuth } from "../contexts/authContext";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={() => (user ? children : <Redirect to="/" />)}
+    ></Route>
+  );
+};
+
+export default PrivateRoute;
