1054b2bbdc455ea4e90988755eefd20ff86fd4b3
segundevs <tushbash@gmail.com>
2022-03-19 14:22:22 -0700

    upgraded packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 //Pages
-import Login from "./pages/Login";
+import AdminLogin from "./pages/AdminLogin";
 import Dashboard from "./pages/Dashboard";
 import Orders from "./pages/Orders";
 import Customers from "./pages/Customers";
 import Vendors from "./pages/Vendors";
+import UsersVendors from "./pages/UsersVendors";
+import UsersSignup from "./pages/UsersSignup";
+import UsersSignin from "./pages/UsersSignin";
+import UsersMeals from "./pages/UsersMeals";
+import VendorPage from "./pages/VendorPage";
+import Home from "./pages/Home";
 
 //Components
-import PrivateRoute from "./components/PrivateRoute";
+import UsersPrivateRoute from "./components/UsersPrivateRoute";
 import ResetPassword from "./pages/ResetPassword";
-import { useAuth } from "./contexts/authContext";
 
 function App() {
-  const { user } = useAuth();
-
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" exact>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/password-reset" element={<ResetPassword />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/orders" element={<Orders />} />
+        <Route path="/admin/customers" element={<Customers />} />
+        <Route path="/admin/vendors" element={<Vendors />} />
+
+        <Route path="/users/register" element={<UsersSignup />} />
+        <Route path="/users/login" element={<UsersSignin />} />
+
+        <Route element={<UsersPrivateRoute />}>
+          <Route path="/users/vendors" element={<UsersVendors />} />
+          <Route path="/users/meals" element={<UsersMeals />} />
+          <Route path="/users/vendor/:id" element={<VendorPage />} />
+        </Route>
+        {/* <Route path="/" exact>
           {!user ? <Login /> : <Dashboard />}
         </Route>
         <Route path="/password-reset">
@@ -36,7 +55,22 @@ function App() {
         <PrivateRoute path="/vendors">
           <Vendors />
         </PrivateRoute>
-      </Switch>
+        <Route path="/sign-up">
+          <UsersSignup />
+        </Route>
+        <Route path="/sign-in">
+          <UsersSignin />
+        </Route>
+        <Route path="/users/vendors">
+          <UsersVendors />
+        </Route>
+        <Route path="/users/meals">
+          <UsersMeals />
+        </Route>
+        <Route path="/users/vendor/:id">
+          <VendorPage />
+        </Route> */}
+      </Routes>
     </div>
   );
 }
