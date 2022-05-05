f78b241cd02f267a9681138060b77c4b5e62b222
segundevs <tushbash@gmail.com>
2022-05-05 07:29:54 -0700

    modified header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,10 +17,10 @@ const Header = () => {
         <nav className={clicked ? "nav_links active" : "nav_links"}>
           <CgClose size={22} className="menu-icon" onClick={()=>{setClicked(prev => !prev)}}/>
           <h2 className='nav_brand'>PEX</h2>
-          <NavLink to="/dashboard" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="grid-outline"></ion-icon> Dashboard</NavLink>
-          <NavLink to="/orders" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="clipboard-outline"></ion-icon> Orders</NavLink>
-          <NavLink to="/customers" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="file-tray-full-outline"></ion-icon> Customers</NavLink>
-          <NavLink to="/vendors" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="folder-outline"></ion-icon> Vendors</NavLink>
+          <NavLink to="/admin/dashboard" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="grid-outline"></ion-icon> Dashboard</NavLink>
+          <NavLink to="/admin/orders" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="clipboard-outline"></ion-icon> Orders</NavLink>
+          <NavLink to="/admin/customers" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="file-tray-full-outline"></ion-icon> Customers</NavLink>
+          <NavLink to="/admin/vendors" className='nav-link' activeClassName="active" onClick={handleClick}><ion-icon name="folder-outline"></ion-icon> Vendors</NavLink>
         </nav>
 
       </header>
